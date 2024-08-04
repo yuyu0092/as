5167dfd8ea9aea68d1bf3e991b4c9a505dfe2c29
松本唯斗 <matsumotoyuito@matsumotoyuitonoiMac.local>
2024-08-05 00:12:11 +0900

    テスト

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".isfadeIn").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+				console.log(bPosition)
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
